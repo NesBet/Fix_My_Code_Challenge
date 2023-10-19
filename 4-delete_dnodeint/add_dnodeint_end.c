00e057eb1ed9a32a312bbffda81c1cf3baa0f41b
Nesh <nehmz254@gmail.com>
2023-10-19 17:47:08 +0300

    Adding files

---
+++ 4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Add a node at the end of a list
+ *
+ * @head: The address of the pointer to the first element of the list
+ * @n: The number to store in the new element
+ *
+ * Return: A pointer to the new element, or NULL on failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        new->prev = NULL;
+        *head = new;
+    }
+    else
+    {
+        dlistint_t *tail = *head;
+        while (tail->next != NULL)
+        {
+            tail = tail->next;
+        }
+        tail->next = new;
+        new->prev = tail;
+    }
+
+    return (new);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Fix-my-code-0
+
+Bonus tasks that involves fixing various codes  
+in different programming languages.
